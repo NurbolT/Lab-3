8999d91f7de05416348a8be24b7db91e11edc639
NurbolIT <tolegenov.n.2705@gmail.com>
2019-09-28 20:51:32 +0600

    first commit

---
+++ WebApplication1/Controllers/CalculatorController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class CalculatorController : Controller
+    {
+        [Route("{firstNumber:int}/{secondNumber:int}")]
+        public IActionResult Sum(int firstNumber, int secondNumber)
+        {
+            //ViewData["number"] = number;
+            //ViewData["result"] = number + 10;
+
+            //ViewData["firstNumber"] = firstNumber;
+            //ViewData["secondNumber"] = secondNumber;
+            //ViewData["result"] = firstNumber + secondNumber;
+
+            ViewData["action"] = RouteData.Values["action"].ToString();
+            ViewData["mark"] = '+';
+            ViewData["firstNumber"] = firstNumber;
+            ViewData["secondNumber"] = secondNumber;
+            ViewData["result"] = firstNumber + secondNumber;
+
+            return View("Result");
+        }
+
+        //[Route("{firstNumber:int}/{secondNumber:int}")]
+        [Route("{firstNumber:int}/{secondNumber:int:min(1)}")]
+        public IActionResult Divide(int firstNumber, int secondNumber)
+        {
+            ViewData["action"] = RouteData.Values["action"].ToString();
+            ViewData["mark"] = '/';
+            ViewData["firstNumber"] = firstNumber;
+            ViewData["secondNumber"] = secondNumber;
+            ViewData["result"] = firstNumber / secondNumber;
+
+            return View("Result");
+        }
+
+
+    }
+}
